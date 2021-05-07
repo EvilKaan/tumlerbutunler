b7a9b6e9ecf140ed894bc98bb456d3e9e43a610f
EvilKaan <83311884+EvilKaan@users.noreply.github.com>
2021-05-07 23:39:08 +0300

    ucgen.py

---
+++ ucgen.py
@@ -0,0 +1,33 @@
+def bütünler(açı):
+    cevapb = int(açı) - int(+360)
+    print(cevapb)
+
+def tümler(açı):
+    cevapt = int(açı) - int(+180)
+    print(cevapt)
+
+
+
+
+print("""
+
+[1] Tümler
+
+[2] Bütünler
+
+
+
+""")
+
+istek = input("İsteğiniz Nedir ? ; ")
+istek = int(istek)
+
+if istek == 1:
+    acevap = input(("Açı kaç derece ; "))
+    acevap = int(acevap)
+    tümler(acevap)
+
+if istek == 2:
+    acevap = input(("Açı kaç derece ; "))
+    acevap = int(acevap)
+    bütünler(acevap)
